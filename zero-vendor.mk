88de06a646785e8e13503ab36cea0224dc9d63ea
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-01-18 17:26:56 +0100

    zero: Bring up for UMI Zero

---
+++ zero-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umi/zero/proprietary/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umi/zero/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umi/zero/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umi/zero/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umi/zero/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umi/zero/proprietary/vendor,system/vendor)
